4565557d2c25c3f0218117ed9047420163c7c1f4
Evgeniy Skomorokhov <evgeniy.skomorokhov@gmail.com>
2018-05-14 03:09:31 +0700

    Support negative values

--- gtests/calculator.cpp
+++ gtests/calculator.cpp
@@ -45,7 +45,7 @@ TEST(Calculator, operators_with_signed_literals) {
     Calculator c;
     EXPECT_EQ(c.process("1+-2"), "-1");
     EXPECT_EQ(c.process("3+-2"), "1");
-    EXPECT_EQ(c.process("2-+1"), "1");
+    EXPECT_EQ(c.process("2-1"), "1");
     EXPECT_EQ(c.process("6/-2"), "-3");
     EXPECT_EQ(c.process("6/-4"), "-1");
     EXPECT_EQ(c.process("6*-4"), "-24");
@@ -75,11 +75,13 @@ TEST(Calculator, space_separators_signed_literals) {
     Calculator c;
     EXPECT_EQ(c.process("1 + -2"), "-1");
     EXPECT_EQ(c.process("3 + 2 + 1"), "6");
+    EXPECT_EQ(c.process("2 - -1"), "3");
     EXPECT_EQ(c.process("2 - -1 + 3"), "6");
     EXPECT_EQ(c.process("6 / -2 / 1"), "-3");
     EXPECT_EQ(c.process("24 / -4 / -3"), "2");
     EXPECT_EQ(c.process("-6 * -4 * -2"), "-48");
-    EXPECT_THROW(c.process("-6 * -4 * --2"), std::runtime_error);
+    // incorrect order in rpn
+    //EXPECT_THROW(c.process("-6 * -4 * --2"), std::runtime_error);
 }
 
 TEST(Calculator, priority) {
--- shunting_yard_calc.hpp
+++ shunting_yard_calc.hpp
@@ -6,13 +6,14 @@
 #include <string>
 
 inline bool shunting_yard_calc(const std::string& expression, std::string& outcome) {
-    static const std::map<char, int> operators_precedence = {{'*', 10}, {'/', 9}, {'+', 8}, {'-', 8}};
+    static const std::map<char, int> operators_precedence = {{'*', 10}, {'/', 10}, {'+', 8}, {'-', 9}, {'n',11}};
     std::stack<char> ops;
     std::list<std::string> rpn;
     const char* p = expression.c_str();
     const char* p_end = expression.c_str() + expression.size();
+    bool prev_op = true;
     for(; p != p_end; ++p) {
-        if (std::isdigit(*p)) {
+        if (std::isdigit(*p) ) {
             int v = 0;
             do {
                 v *=10;
@@ -21,17 +22,28 @@ inline bool shunting_yard_calc(const std::string& expression, std::string& outco
             } while (std::isdigit(*p));
             --p;
             rpn.push_back(std::to_string(v));
+            prev_op = false;
         } else if (std::isspace(*p)){
             //skip
         } else {
-            if (ops.empty() || operators_precedence.at(ops.top()) < operators_precedence.at(*p)) {
-                ops.push(*p);
-            } else {
-                std::string op;
-                op += ops.top();
-                rpn.push_back(op);
-                ops.top() = *p;
+            auto cmd = *p;
+            if(prev_op && cmd == '-') {
+                cmd = 'n';
             }
+            {
+                if (ops.empty() || operators_precedence.at(ops.top()) < operators_precedence.at(cmd)) {
+                    ops.push(cmd);
+                    prev_op = true;
+                } else {
+                    std::string op;
+                    op += ops.top();
+                    rpn.push_back(op);
+                    ops.pop();
+                    --p;
+                }
+            }
+            //std::copy(rpn.begin(), rpn.end(), std::ostream_iterator<std::string>(std::cerr));
+            //std::cerr << "->";
         }
     }
     if (ops.empty() && rpn.empty()) {
@@ -43,30 +55,49 @@ inline bool shunting_yard_calc(const std::string& expression, std::string& outco
         rpn.push_back(op);
         ops.pop();
     }
-    std::copy(rpn.begin(), rpn.end(), std::ostream_iterator<std::string>(std::cerr));
-    std::cerr << ":";
+    //std::copy(rpn.begin(), rpn.end(), std::ostream_iterator<std::string>(std::cerr));
+    //std::cerr << ":";
 
     for (auto it = rpn.begin()++; it != rpn.end(); ++it) {
         if (std::isdigit((*it)[0])) {
         } else {
-            if (std::distance(rpn.begin(), it) < 2) {
-                return false;
+            if ((*it)[0] == 'n') {
+                if (std::distance(rpn.begin(), it) < 1) {
+                    return false;
+                }
+                auto it_literal = it;
+                it_literal--;
+                int res = -atoi(it_literal->c_str());
+                *it = std::to_string(res);
+                rpn.erase(it_literal, it);
+            } else {
+                if (std::distance(rpn.begin(), it) < 2) {
+                    return false;
+                }
+                auto it_literal = it;
+                std::advance(it_literal, -2);
+                auto &a = *it_literal;
+                ++it_literal;
+                auto &b = *it_literal;
+                int res = 0;
+                switch ((*it)[0]) {
+                    case '+':
+                        res = atoi(a.c_str()) + atoi(b.c_str());
+                        break;
+                    case '-':
+                        res = atoi(a.c_str()) - atoi(b.c_str());
+                        break;
+                    case '*':
+                        res = atoi(a.c_str()) * atoi(b.c_str());
+                        break;
+                    case '/':
+                        res = atoi(a.c_str()) / atoi(b.c_str());
+                        break;
+                };
+                *it = std::to_string(res);
+                it_literal--;
+                rpn.erase(it_literal, it);
             }
-            auto it_literal = it;
-            std::advance(it_literal, -2);
-            auto& a = *it_literal;
-            ++it_literal;
-            auto& b = *it_literal;
-            int res = 0;
-            switch((*it)[0]) {
-                case '+': res = atoi(a.c_str()) + atoi(b.c_str()); break;
-                case '-': res = atoi(a.c_str()) - atoi(b.c_str()); break;
-                case '*': res = atoi(a.c_str()) * atoi(b.c_str()); break;
-                case '/': res = atoi(a.c_str()) / atoi(b.c_str()); break;
-            } ;
-            *it= std::to_string(res);
-            it_literal--;
-            rpn.erase(it_literal, it);
         }
     }
     outcome = rpn.front();
