e80cbf7e64dc210c18775039f50cab47c9e9f98f
Evgeniy Skomorokhov <evgeniy.skomorokhov@gmail.com>
2018-05-19 22:06:22 +0700

    Fix calculation order

--- server.hpp
+++ server.hpp
@@ -85,10 +85,17 @@ private:
     TCPConnection(boost::asio::io_service& io_service, ThreadPool& workers, Processor& processor)
             : socket_(io_service)
             , workers_(workers)
-            , processor_(processor) {
+            , processor_(processor)
+            , is_calculating_(ATOMIC_FLAG_INIT) {
     }
     void handle_write(const std::shared_ptr<std::string>& /*message_ptr*/) {
         //std::cout << "written " << *message_ptr << "\n";
+        bool expected = false;
+        if (!expressions_.empty() && is_calculating_.compare_exchange_strong(expected, true,
+                                        std::memory_order_release,
+                                        std::memory_order_relaxed)) {
+            process(std::move(expressions_));
+        }
     }
 
     void handle_read(const boost::system::error_code& e, const std::size_t bytes_transferred) {
@@ -116,20 +123,26 @@ private:
                 enter_character_ind = bytes_transferred - 1;
             }
             if (!reqs.empty()) {
-                std::copy(reqs[0].begin(), reqs[0].begin() + reqs[0].size(), std::back_inserter(expression_));
-                reqs[0] = std::move(expression_);
+                std::copy(reqs[0].begin(), reqs[0].begin() + reqs[0].size(), std::back_inserter(pending_expression_));
+                reqs[0] = std::move(pending_expression_);
             }
             std::copy(buffer_.begin() + (enter_character_ind + 1), buffer_.begin() + bytes_transferred,
-                      std::back_inserter(expression_));
-            //std::cerr << "exp_rest:" << expression_ << "\n";
+                      std::back_inserter(pending_expression_));
+            //std::cerr << "exp_rest:" << pending_expression_ << "\n";
+            for (auto& expression: reqs) {
+                expressions_.push_back(std::move(expression));
+            }
+            bool expected = false;
+            if (!expressions_.empty() && is_calculating_.compare_exchange_strong(expected, true,
+                                        std::memory_order_release,
+                                        std::memory_order_relaxed)) {
+                    process(std::move(expressions_));
+            }
             if (e != boost::asio::error::eof) {
                 socket_.async_read_some(boost::asio::buffer(buffer_),
                                               boost::bind(&TCPConnection::handle_read, shared_from_this(),
                                                           boost::asio::placeholders::error,
                                                           boost::asio::placeholders::bytes_transferred));
-            }
-            if (!reqs.empty()) {
-                process(std::move(reqs), e == boost::asio::error::eof);
             }
         } else {
             log_debug("error: ", e.message());
@@ -137,16 +150,13 @@ private:
         // destroy object at the end
     }
 
-    void process(std::vector<std::deque<char> >&& vec_messages, bool stop = false) {
-        workers_.enqueue([stop](const std::weak_ptr<TCPConnection>& this_weak_ptr, std::vector<std::deque<char> > vec_messages) {
+    void process(std::vector<std::deque<char> >&& vec_messages) {
+        workers_.enqueue([](const std::weak_ptr<TCPConnection>& this_weak_ptr, std::vector<std::deque<char> > vec_messages) {
             for (auto& message: vec_messages) {
-                if (message.empty()) {
-                    continue;
-                }
                 auto this_ptr = this_weak_ptr.lock();
                 if (!this_ptr) {
                     log_info("Interrupt calculation due to died connection");
-                    return;
+                    break;
                 }
                 std::string exp;
                 exp.append(message.begin(), message.begin() + message.size());
@@ -156,8 +166,14 @@ private:
                 boost::asio::async_write(this_ptr->socket_, boost::asio::buffer(*res),
                                          boost::bind(&TCPConnection::handle_write, this_ptr, res));
             }
-            if (stop) {
-                return;
+            if (auto this_ptr = this_weak_ptr.lock()) {
+                bool expected = true;
+                this_ptr->is_calculating_.compare_exchange_strong(expected, false,
+                                        std::memory_order_release,
+                                        std::memory_order_relaxed);
+                std::shared_ptr<std::string> res = std::make_shared<std::string>();
+                boost::asio::async_write(this_ptr->socket_, boost::asio::buffer(*res),
+                                         boost::bind(&TCPConnection::handle_write, this_ptr, res));
             }
         }, std::weak_ptr<TCPConnection>(shared_from_this()), std::move(vec_messages));
     }
@@ -165,9 +181,11 @@ private:
     boost::asio::ip::tcp::socket socket_;
     ThreadPool& workers_;
     boost::array<char, 8192> buffer_;
-    std::deque<char> expression_;
+    std::deque<char> pending_expression_;
+    std::vector<std::deque<char>> expressions_;
     Processor& processor_;
     std::deque<char> output_buffer_;
+    std::atomic<bool> is_calculating_;
 };
 
 class TCPServer
